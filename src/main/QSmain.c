c5f7e0227e322705b3db81545260af51df2d9543
Greg Ostroy <usheranchel@yahoo.com>
2017-02-24 18:20:34 -0500

    automated unit Tests

--- src/main/QSmain.c
+++ src/main/QSmain.c
@@ -11,12 +11,8 @@ Copyright (C) 2017 Greg Ostroy see full notice in [installation path]/QuadraticS
 */
 
 #include<stdio.h>
-<<<<<<< HEAD
 #include "../Input/parser.h"
 #include "../Validator/validator.h"
-=======
-#include "../InputParser/parser.h"
->>>>>>> d0cb8795e5f40a91fe5bda5399cb6ff9f2c28e3b
 #include "../Printer/printer.h"
 #include "../QSolver/quadraticFormula.h"
 #include "../SquareRt/squareRoot.h"
@@ -28,23 +24,17 @@ int main()
 	{
 		printf("If you would like to solve a quadratic equation type 'y', or type any key to quit.\n");
 		{
-<<<<<<< HEAD
 			char input='n';
 			if(fgets(&input,16,stdin)==NULL)
 			{
 				printf("read error");
 				return 1;
 			}
-=======
-			char input ='n';
-			scanf("%c",&input);
->>>>>>> d0cb8795e5f40a91fe5bda5399cb6ff9f2c28e3b
 			if(input=='y'|| input=='Y')
 			{
 				float a=0.0;
 				float b=0.0;
 				float c=0.0;
-<<<<<<< HEAD
 				char floatInput[64];
 				printf("For the quadratic equation ax^2 + bx +c =0, please enter the values for coefficients a,b,and c, as three white space separated positive numbers\n");
 				if(fgets(floatInput,64,stdin)==NULL)
@@ -84,42 +74,6 @@ int main()
 						}
 					}
 				}				
-=======
-				int valid = 1;
-				while(valid!=0)//keep prompting the user until they enter valid data
-				{
-					char as[32];
-					char bs[32];
-					char cs[32];
-					printf("For the quadratic equation ax^2 + bx +c =0, please enter the values for coefficients a,b,and c, as three white space separated positive numbers\n");
-					char dump[2];//trailing return char 
-					scanf("%s%s%s%c",as,bs,cs,dump);
-					
-					valid=parseCoefficients(as,bs,cs,&a,&b,&c);//convert string input to floats
-					if(valid !=0)//invalid input
-					printf("You did not enter valid numbers\n");
-				}
-				double sqrt=0.0;
-				int sqrtCode=squareRoot(a,b,c,&sqrt);//determine sqrt(b^2 -4ac)
-				if(sqrtCode==-1)//imaginary roots
-					printf("The equation has no real roots\n");
-				else//real roots
-				{
-					if(sqrtCode==0)//one root
-					{
-						float root1=0.0;
-						quadraticFormula(a,b,sqrt,&root1,&root1);//determine b-sqrt(b^2 -4ac)/2a
-						printRoots(&root1,&root1);
-					}
-					else//two roots
-					{
-						float root1=0.0;
-						float root2=0.0;
-						quadraticFormula(a,b,sqrt,&root1,&root2);
-						printRoots(&root1,&root2);
-					}
-				}
->>>>>>> d0cb8795e5f40a91fe5bda5399cb6ff9f2c28e3b
 			}
 			else
 				finished=1;
